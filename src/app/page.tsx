9e04f47993384ad549acc2e8fbf407401319cddb
Madan Bhat <imadanbhat@gmail.com>
2024-10-27 13:57:36 +0530

    feat: new ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ export default function Page() {
             <BlurFade delay={BLUR_FADE_DELAY}>
               <Avatar className="size-28 border">
                 <AvatarImage alt={DATA.name} src={DATA.avatarUrl} />
-                <AvatarFallback>{DATA?.initials}</AvatarFallback>
+                <AvatarFallback>{DATA.initials}</AvatarFallback>
               </Avatar>
             </BlurFade>
           </div>
@@ -142,6 +142,7 @@ export default function Page() {
                   title={project.title}
                   description={project.description}
                   dates={project.dates}
+                  image={project.image}
                   tags={project.technologies}
                   links={project.links}
                 />
